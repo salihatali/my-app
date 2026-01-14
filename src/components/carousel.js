cd25f8ea1497070c4248c85c9db017ecefb5c5d3
Salih Atalı <125149396+salihatali34@users.noreply.github.com>
2026-01-14 18:19:45 +0300

    fnish

---
+++ src/components/carousel.js
@@ -0,0 +1,33 @@
+"use client";
+import { useState } from "react";
+import "../styles/carousel.css";
+
+export default function Carousel() {
+  const items = [
+    "https://picsum.photos/300/200",
+    "https://picsum.photos/300/200/?2",
+    "https://picsum.photos/300/200/?3",
+  ];
+  const [image, setImage] = useState(0);
+
+  const nextImage = () => {
+    setImage((p) => (p === items.length - 1 ? 0 : p + 1));
+  };
+
+  const prevImage = () => {
+    setImage((p) => (p === 0 ? items.length - 1 : p - 1));
+  };
+
+  return (
+<div className="carousel">
+
+  <img src={items[image]} className="carousel-image"/>
+
+  <div className="carousel-buttons">
+    <button onClick={prevImage}>◀</button>
+    <button onClick={nextImage}>▶</button>
+  </div>
+</div>
+
+  );
+}
